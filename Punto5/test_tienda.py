806a6d81c1c82cb7f6b4b560b419eb0f8a59fb62
MarianoGomesOpitz <101834507+MarianoGomesOpitz@users.noreply.github.com>
2024-09-08 17:43:07 -0300

    Ajuste pequeño detalle

--- Punto5/test_tienda.py
+++ Punto5/test_tienda.py
@@ -26,15 +26,3 @@ def test_calcular_total_carrito(tienda_con_productos):
     carrito= ["leche", "queso", "pan", "jamon_crudo","galleta_salvado"]
     total = tienda_con_productos.calcular_total_carrito(carrito)
     assert round(total,2) == 12.35  ##El 12.35 es la sumatoria de todos los productos con el descuento correspondiente
-    
-
-
-## Una situación de desarrollo en la que las pruebas sean de integración ascendente es uno en el que se desarrolla 
-## cada módulo de nivel inferior y al tenerlo probado hasta un punto satisfactorio, se prosigue con un módulo de nivel 
-## superior que utiliza ese módulo inferior.
-## Un ejemplo puede ser el de este mismo TP. Primero se desarrolla el objeto Producto con todos las funcionalidades que le
-## corresponden, se las prueba de manera exhaustiva para corrobar que funcione correctamente, y encontrar y solucionar posibles
-## errores de funcionamiento. Luego se continua con la Tienda, que utiliza al Producto y a sus funciones para sí mismas.
-## Se realizan pruebas que verifiquen su funcionamiento, y al ya saber que el Producto funciona correctamente, los posibles
-## errores que pueden aparecer son del funcionamiento de la Tienda, o de implementación del Producto en la Tienda. Para el final
-## realizar la función de calcular la compra que utiliza todas las funciones de la Tienda.
